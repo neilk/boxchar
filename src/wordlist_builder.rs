c1dcb6584c12d3175f6a825550f377fdf2ed4d46
Neil Kandalgaonkar <neilk@neilk.net>
2025-09-28 14:37:38 -0700

    wordlist builder

---
+++ src/wordlist_builder.rs
@@ -0,0 +1,112 @@
+use clap::Parser;
+use std::cmp::{min, Ordering};
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+/**
+ * Build the standard word-list for boxchar, which will be a list of words which are playable, along with
+ * how frequent they are in english. The word-list will then be sorted by frequency, which will make it 
+ * easier to display "good" solutions first.
+ * 
+ * We want to create the word list from two sources, which are both large text files.
+ *    - the collins scrabble words, a newline-delimited text file, one word per line, e.g.
+ *          AA
+ *          AAH
+ *          AAHED
+ *          AAHING
+ *          AAHS
+ *    - a dump from google ngrams, a tab-separated newline-delimited text file, with integers representing 
+ *      how frequent the word is. Larger is more frequent. The largest number here is ~ 2**36
+ *          a       14219615690
+ *          a!      196012
+ *          a"      84
+ *          a'      47713
+ * 
+ * We will iterate through both files simultaneously, outputing lines as appropriate, e.g. 
+ *          aba 114620
+ *          abac 5914
+ *          abacas 423
+ *          abaci 41132
+ *          aback 1138210
+ *
+ * We expect the user to then sort the file appropriately with shell tools, e.g.
+ *     $ cargo run wordlist-builder -- --frequencies data/google-ngrams-words-all.txt > /tmp/wordlist.txt 
+ *     $ sort -k 2,2rn -k 1 /tmp/wordlist.txt > data/wordlist.txt
+ * 
+ */
+
+#[derive(Parser)]
+#[command(name = "wordlist-builder")]
+#[command(about = "Builds the dictionary wordlist for Boxchar from Google NGrams and the Scrabble dictionary")]
+struct Args {
+    #[arg(long)]
+    frequencies: String,
+
+    #[arg(long, default_value = "data/collins-scrabble-words-2019.txt")]
+    scrabble: String
+}
+
+const MINIMUM_LENGTH: usize = 3;
+
+/** 
+ * Word has to be of minimum length, and have no immediately doubled letters. BUT is okay, BUTT is not.
+ * It also has to be all lowercase a-z letters, but we assume the Scrabble dictionary has that property already.
+ */
+fn is_playable_word(word: &str) -> bool {
+    if word.len() < MINIMUM_LENGTH {
+        return false;
+    } 
+    
+    !word.chars().zip(word.chars().skip(1)).any(|(a, b)| a == b)
+}
+
+fn main() -> std::io::Result<()> {
+    env_logger::init();
+    let args = Args::parse();
+
+    let scrabble_path = Path::new(&args.scrabble);
+    let frequencies_path = Path::new(&args.frequencies);
+
+    let scrabble_file = File::open(scrabble_path)?;
+    let scrabble_reader = BufReader::new(scrabble_file);
+    let mut scrabble_lines = scrabble_reader.lines();
+
+    let frequencies_file = File::open(frequencies_path)?;
+    let frequencies_reader = BufReader::new(frequencies_file);
+    let mut frequencies_lines = frequencies_reader.lines();
+
+    let mut frequencies_line_current = frequencies_lines.next();
+    let mut scrabble_line_current = frequencies_lines.next();
+
+    // Iterate through both of these very large files at once
+    while let (Some(frequencies_line), Some(scrabble_line)) = (&frequencies_line_current, &scrabble_line_current) {
+        let scrabble_word: String = scrabble_line.as_ref().unwrap().clone().to_lowercase();
+        let mut frequencies_split = frequencies_line.as_ref().unwrap().split_whitespace();
+        let frequencies_word: &str = frequencies_split.next().unwrap();
+
+        // The largest frequency in this file is about 2**35, so u64 should do it. 
+        let frequency: u64 = frequencies_split.next().unwrap().parse().unwrap();
+        // However, to save a few bytes later when we pack it, we're going to assume the maximum "frequency_score" is just 31.
+        // There are only a few super-short words which are above 31 anyway.
+        let frequency_score = min(frequency.ilog2(), 31);
+
+        match frequencies_word.cmp(&scrabble_word) {
+            Ordering::Equal => {
+                if is_playable_word(frequencies_word) {
+                    println!("{} {}", frequencies_word, frequency_score);
+                }
+                frequencies_line_current = frequencies_lines.next();
+                scrabble_line_current = frequencies_lines.next();
+            }
+            Ordering::Less => {
+                frequencies_line_current = frequencies_lines.next();
+            }
+            Ordering::Greater => {
+                scrabble_line_current = scrabble_lines.next();
+            }
+        }
+    }
+
+    Ok(())
+}
