70e611387d017e1ab21ffcb8351c7a8e0894fcec
Neil Kandalgaonkar <neilk@neilk.net>
2025-10-03 13:36:30 -0700

    board path

--- tests/board.rs
+++ tests/board.rs
@@ -1,8 +1,8 @@
 use boxchar::board::Board;
 
 #[test]
-fn test_from_path_data_game() {
-    let board = Board::from_path("data/game.txt").unwrap();
+fn test_from_path() {
+    let board = Board::from_path("data/board.txt").unwrap();
     
     assert_eq!(board.sides.len(), 4);
     assert!(!board.digraphs.is_empty());
